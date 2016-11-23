c28e62f7594e125f20edb1603e1c755c34449284
Pratheek Hegde <ptk609@gmail.com>
2016-11-23 22:54:58 +0530

    refactor: exposed an API endpoint with express

--- server.js
+++ server.js
@@ -1,75 +1,89 @@
 var fs = require('fs'),
   request = require('request'),
   cheerio = require('cheerio'),
-  async = require('async')
+  async = require('async'),
+  express = require('express');
 
-var EventEmitter = require("events").EventEmitter;
-var event = new EventEmitter();
 var semesters = [];
 
-//scrap results from fastvturesults.com
-url = 'http://www.fastvturesults.com/result/4mw12cs064'
-console.info("Retriving sem marks ...");
-request(url, function(err, response, html) {
+var app = express();
 
-  var $ = cheerio.load(html);
-  $('#scell .success,#scell .danger').each(function() {
-    // console.info("sem : " + $(this).find('td').eq(0).text().trim() +
-    //     ",attempt : " + $(this).find('td').eq(1).text().trim() +
-    //     ",total_marks : " + $(this).find('td').eq(2).text().trim() +
-    //     ",results : " + $(this).find('td').eq(3).text().trim() +
-    //     ",percentage : " + $(this).find('td').eq(4).text().trim()
-    // );
+app.get('/resuts/:usn', function(req, res) {
 
-    var json = {
-      sem: $(this).find('td').eq(0).text().trim(),
-      attempt: $(this).find('td').eq(1).text().trim(),
-      total_marks: $(this).find('td').eq(2).text().trim(),
-      results: $(this).find('td').eq(3).text().trim(),
-      percentage: $(this).find('td').eq(4).text().trim(),
-      marks: $(this).find('td').eq(7).find('a').attr('href')
-    };
+  //scrap results from fastvturesults.com
+  //console.log(req.params.usn)
+  url = 'http://www.fastvturesults.com/result/' + req.params.usn;
+  //console.info("Retriving sem marks ...");
 
-    semesters.push(json);
-  });
-  console.info("Sem marks retrived ...");
-  event.emit('update');
-});
-
-event.on('update', function() {
-  console.info("Retriving individual sem marks ...");
-
-  async.eachOfSeries(semesters, function(sem, key, callback) {
-      request(sem.marks, function(error, response, body) {
-        if (!error) {
-          var semData = [];
-          var $ = cheerio.load(body);
-
+  async.waterfall([
+    function(fetchInitialResultscallback) {
+        request(url, function(err, response, html) {
+        var $ = cheerio.load(html);
           $('#scell .success,#scell .danger').each(function() {
+            // console.info("sem : " + $(this).find('td').eq(0).text().trim() +
+            //     ",attempt : " + $(this).find('td').eq(1).text().trim() +
+            //     ",total_marks : " + $(this).find('td').eq(2).text().trim() +
+            //     ",results : " + $(this).find('td').eq(3).text().trim() +
+            //     ",percentage : " + $(this).find('td').eq(4).text().trim()
+            // );
 
-            var semJson = {
-              sub_name: $(this).find('td').eq(0).text().trim(),
-              sub_code: $(this).find('td').eq(1).text().trim(),
-              ia_marks: $(this).find('td').eq(2).text().trim(),
-              ext_marks: $(this).find('td').eq(3).text().trim(),              
-              total_marks: $(this).find('td').eq(4).text().trim(),
-              results: $(this).find('td').eq(5).text().trim()
-            }
+            var json = {
+              sem: $(this).find('td').eq(0).text().trim(),
+              attempt: $(this).find('td').eq(1).text().trim(),
+              total_marks: $(this).find('td').eq(2).text().trim(),
+              results: $(this).find('td').eq(3).text().trim(),
+              percentage: $(this).find('td').eq(4).text().trim(),
+              marks: $(this).find('td').eq(7).find('a').attr('href')
+            };
 
-            semData.push(semJson);
-            //console.log("---next-sem---");
+            semesters.push(json);
           });
-          sem.marks = semData;
-          semesters[key].marks = semData
-        }
-        fs.writeFile('output.json', JSON.stringify(semesters, null, 4), function(err) {
-          if (err) console.error(err.message);
-        })
+          fetchInitialResultscallback(null, semesters);
       });
-      callback();
     },
-    function(err) {
-      if (err) console.error(err.message);
-      console.log('Output.json file created ...');
-    });
+    function(semesters, fetchIndividualResultscallback) {
+      // arg1 now equals 'one' and arg2 now equals 'two'
+      //console.info("Retriving individual sem marks ...");
+      async.map(semesters, function(sem, eachOfSeriesCallback) {
+          request(sem.marks, function(error, response, body) {
+            if (!error) {
+              var semData = [];
+              var $ = cheerio.load(body);
+
+              $('#scell .success,#scell .danger').each(function() {
+
+                var semJson = {
+                  sub_name: $(this).find('td').eq(0).text().trim(),
+                  sub_code: $(this).find('td').eq(1).text().trim(),
+                  ia_marks: $(this).find('td').eq(2).text().trim(),
+                  ext_marks: $(this).find('td').eq(3).text().trim(),
+                  total_marks: $(this).find('td').eq(4).text().trim(),
+                  results: $(this).find('td').eq(5).text().trim()
+                }
+
+                semData.push(semJson);
+                //console.log("---next-sem---");
+              });
+              sem.marks = semData;
+              //semesters[key].marks = semData
+            }          
+            eachOfSeriesCallback(null,sem);
+          });
+        },
+        function(err,results) { 
+          if (err) console.error(err.message);
+          //console.log('Output.json file created ...');
+        fetchIndividualResultscallback(null, results);
+        });
+    }
+  ], function(err, result) {
+    // result now equals 'done'
+    res.setHeader('Content-Type', 'application/json');
+    res.json(result, null, 3);
+    semesters = [];
+  });
+
 });
+
+app.listen(3000);
+console.log('Listening on port 3000...');
